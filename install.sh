692f461d3ed0f880aab6e3b2f717203c393e3af4
Nicolas COCHIN <nicolas.cochin@lifen.fr>
2023-12-21 17:35:33 +0100

    Install GitHub CLI

--- install.sh
+++ install.sh
@@ -1,3 +1,18 @@
 #!/bin/bash 
 
 [ ! -f /etc/arch-release ] && echo "This script only runs on arch linux" && exit 1
+
+# $1 == cmd
+function is_installed {
+  if command -v $1 > /dev/null; then 
+    echo "$1 is already installed"
+    true
+  else 
+    false
+  fi
+}
+
+! is_installed gh && (
+  echo "Installing GitHub CLI"
+  sudo pacman -S github-cli
+)
